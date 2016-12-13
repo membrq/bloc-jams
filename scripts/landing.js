51b23c092e5a3b5cda93344d6093cc97dcd9ddfe
Marie-Elise <membrq@gmail.com>
2016-12-12 21:14:54 -0600

    Checkpoint 23 (Events) completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,8 @@
-var animatePoints = function() {
-	var points = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
+
+var animatePoints = function(points) {
 				
-	var revealPoint = function(points[index]) {
+	var revealPoint = function(index) {
 	for (var i = 0; i < 3; i++) {
 		points[index].style.opacity = 1;
 		points[index].style.transform = "scaleX(1) translateY(0)";
@@ -11,6 +12,19 @@ var animatePoints = function() {
 };
 revealPoint();
 };
-			
-animatePoints();
+
+window.onload = function() {
+	if (window.innerHeight > 950) {
+		animatePoints(pointsArray);
+	}
+	
+	var sellingPoints = document.getElementsByClassName('selling-points')[0];
+	var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+	
+	window.addEventListener("scroll", function(event) {
+		if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+			animatePoints(pointsArray);
+		}
+	});
+}
 
