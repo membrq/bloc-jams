29c482896f2f43584122d7a94c76abeda9a36dbf
Marie-Elise <membrq@gmail.com>
2016-12-20 11:30:32 -0600

    Assignment 25 completed

--- scripts/album.js
+++ scripts/album.js
@@ -18,7 +18,7 @@ var albumMarconi = {
 	artist: 'Guglielmo Marconi',
 	label: 'EM',
 	year: '1909',
-	albumArtUrl: 'assets/images/album_Covers/20.png',
+	albumArtUrl: 'assets/images/album_covers/20.png',
 	songs: [
 		{title: 'Hello, Operator?', duration: '1:01'},
 		{title: 'Ring, ring, ring', duration: '5:01'},
@@ -28,37 +28,67 @@ var albumMarconi = {
 	]
 };
 
-var createSongRow = function(songNumber, songName, songLength) {
-	var template =
-		'<tr class = "album-view-song-item">'
-	+ 	'	<td class="song-item-number>' + songNumber + '</td>'
-	+ 	'	<td class="song-item-title>' + songName + '</td>'
-	+ 	'	<td class="song-item-duration>' + songLength + '</td>'
-	+ '</tr>'
-	;
-	
-	return template;
+var albumChristmas = {
+	title: 'It\'s Christmas',
+	artist: 'Santa\'s Elves',
+	label: 'North Pole',
+	year: '1950',
+	albumArtUrl: 'assets/images/album_covers/02.png',
+	songs: [
+		{title: 'Ho, Ho, Ho!', duration: '4:44'},
+		{title: 'Chestnuts', duration: '3:33'},
+		{title: 'It\s Freezing and I Can\'t Feel My Face', duration: '9:99'},
+		{title: 'Rudolph', duration: '5:55'},
+		{title: 'Happy Hanukkah', duration: '1:11'}
+	]
 };
 
-var setCurrentAlbum = function(album) {
+
+window.onload = function() {
 	var albumTitle = document.getElementsByClassName('album-view-title')[0];
 	var albumArtist = document.getElementsByClassName('album-view-artist')[0];
 	var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
 	var albumImage = document.getElementsByClassName('album-cover-art')[0];
 	var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 	
-	albumTitle.firstChild.nodeValue = album.title;
-	albumArtist.firstChild.nodeValue = album.artist;
-	albumReleaseInfo.firstChild.nodeValue = album.year;
-	albumImage.setAttribute('src', album.albumArtUrl);
+	var createSongRow = function(songNumber, songName, songLength) {
+		var template =
+			'<tr class = "album-view-song-item">'
+		+ 	'	<td class="song-item-number>' + songNumber + '</td>'
+		+ 	'	<td class="song-item-title>' + songName + '</td>'
+		+ 	'	<td class="song-item-duration>' + songLength + '</td>'
+		+ 	'</tr>'
+		;
+
+		return template;
+	};
+
+	var setCurrentAlbum = function(album) {
+
+		albumTitle.firstChild.nodeValue = album.title;
+		albumArtist.firstChild.nodeValue = album.artist;
+		albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
+		albumImage.setAttribute('src', album.albumArtUrl);
+
+		albumSongList.innerHTML = '';
+
+		for (var i = 0; i < album.songs.length; i++) {
+			albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+		}
+	};
+
 	
-	albumSongList.innerHTML;
+	setCurrentAlbum(albumPicasso);
 	
-	for (var i = 0; i < album.songs.length; i++) {
-		albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
-	}
+	var albums = [albumPicasso, albumMarconi, albumChristmas];
+	var index = 1;
+	
+	albumImage.addEventListener('click', function(event) {
+		setCurrentAlbum(albums[index]);
+		index++;
+		if (index == albums.length) {
+			index = 0;
+		}
+	});
 };
 
-window.onload = function() {
-	setCurrentAlbum(albumPicasso);
-};
